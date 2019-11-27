9ffa31410efd5477c4bdd147e0eccce869a16b89
Keeton T. Feavel <keetonfeavel@gmail.com>
2019-11-27 15:40:55 -0500

    Began working on shell

---
+++ kernel/shell/shell.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file shell.cpp
+ * @author Keeton Feavel (keetonfeavel@cedarville.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2019-11-27
+ * 
+ * @copyright Copyright Keeton Feavel et al (c) 2019
+ * 
+ */
+
+#include <shell/shell.hpp>
+
+void px_print_shell_indicator() {
+    px_kprint("basch# ");
+}
+
