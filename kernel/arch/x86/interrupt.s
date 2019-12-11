10c3c1b7df9b6db8861c2bc0de1c4f8c5ee9dc6d
Keeton T. Feavel <keetonfeavel@gmail.com>
2019-12-11 01:16:08 -0500

    Cleaned up interrupt ASM

--- kernel/arch/x86/interrupt.s
+++ kernel/arch/x86/interrupt.s
@@ -3,7 +3,7 @@
 .extern px_irq_handler
 
 # Common ISR code
-isr_common_stub: 
+isr_common_stub:
     # 1. Save CPU state
     pusha           # Pushes edi,esi,ebp,esp,ebx,edx,ecx,eax
     movw %ds,%ax    # Lower 16-bits of eax = ds.
@@ -31,7 +31,7 @@ isr_common_stub:
 
 # Common IRQ code. Identical to ISR code except for the 'call' 
 # and the 'pop ebx'
-irq_common_stub: 
+irq_common_stub:
     pusha
     movw %ds,%ax
     pushl %eax
@@ -58,319 +58,328 @@ irq_common_stub:
 # don't, so we will push a dummy error code for those which don't, so that
 # we have a consistent stack for all of them.
 
-# First make the ISRs global
-.global isr0
-.global isr1
-.global isr2
-.global isr3
-.global isr4
-.global isr5
-.global isr6
-.global isr7
-.global isr8
-.global isr9
-.global isr10
-.global isr11
-.global isr12
-.global isr13
-.global isr14
-.global isr15
-.global isr16
-.global isr17
-.global isr18
-.global isr19
-.global isr20
-.global isr21
-.global isr22
-.global isr23
-.global isr24
-.global isr25
-.global isr26
-.global isr27
-.global isr28
-.global isr29
-.global isr30
-.global isr31
-# IRQs
-.global irq0
-.global irq1
-.global irq2
-.global irq3
-.global irq4
-.global irq5
-.global irq6
-.global irq7
-.global irq8
-.global irq9
-.global irq10
-.global irq11
-.global irq12
-.global irq13
-.global irq14
-.global irq15
-
+# All of the following Interrupt Service Routines are
+# exception handlers. As such, they push the error code
+# along with their exception (interrupt) number. If an
+# exception has an error code, $0 will not be pushed as
+# the CPU already does that for us.
+#
 # 0: Divide By Zero Exception
-isr0: 
+.global isr0
+isr0:
     push $0
     push $0
     jmp isr_common_stub
 
 # 1: Debug Exception
-isr1: 
+.global isr1
+isr1:
     push $0
     push $1
     jmp isr_common_stub
 
 # 2: Non Maskable Interrupt Exception
-isr2: 
+.global isr2
+isr2:
     push $0
     push $2
     jmp isr_common_stub
 
 # 3: Int 3 Exception
-isr3: 
+.global isr3
+isr3:
     push $0
     push $3
     jmp isr_common_stub
 
 # 4: INTO Exception
-isr4: 
+.global isr4
+isr4:
     push $0
     push $4
     jmp isr_common_stub
 
 # 5: Out of Bounds Exception
-isr5: 
+.global isr5
+isr5:
     push $0
     push $5
     jmp isr_common_stub
 
 # 6: Invalid Opcode Exception
-isr6: 
+.global isr6
+isr6:
     push $0
     push $6
     jmp isr_common_stub
 
 # 7: Coprocessor Not Available Exception
-isr7: 
+.global isr7
+isr7:
     push $0
     push $7
     jmp isr_common_stub
 
 # 8: Double Fault Exception (With Error Code!)
-isr8: 
+.global isr8
+isr8:
     push $8
     jmp isr_common_stub
 
 # 9: Coprocessor Segment Overrun Exception
-isr9: 
+.global isr9
+isr9:
     push $0
     push $9
     jmp isr_common_stub
 
 # 10: Bad TSS Exception (With Error Code!)
-isr10: 
+.global isr10
+isr10:
     push $10
     jmp isr_common_stub
 
 # 11: Segment Not Present Exception (With Error Code!)
-isr11: 
+.global isr11
+isr11:
     push $11
     jmp isr_common_stub
 
 # 12: Stack Fault Exception (With Error Code!)
-isr12: 
+.global isr12
+isr12:
     push $12
     jmp isr_common_stub
 
 # 13: General Protection Fault Exception (With Error Code!)
-isr13: 
+.global isr13
+isr13:
     push $13
     jmp isr_common_stub
 
 # 14: Page Fault Exception (With Error Code!)
-isr14: 
+.global isr14
+isr14:
     push $14
     jmp isr_common_stub
 
 # 15: Reserved Exception
-isr15: 
+.global isr15
+isr15:
     push $0
     push $15
     jmp isr_common_stub
 
 # 16: Floating Point Exception
-isr16: 
+.global isr16
+isr16:
     push $0
     push $16
     jmp isr_common_stub
 
 # 17: Alignment Check Exception (With Error Code!)
-isr17: 
+.global isr17
+isr17:
     push $17
     jmp isr_common_stub
 
 # 18: Machine Check Exception
-isr18: 
+.global isr18
+isr18:
     push $0
     push $18
     jmp isr_common_stub
 
 # 19: Reserved
-isr19: 
+.global isr19
+isr19:
     push $0
     push $19
     jmp isr_common_stub
 
 # 20: Reserved
-isr20: 
+.global isr20
+isr20:
     push $0
     push $20
     jmp isr_common_stub
 
 # 21: Reserved
-isr21: 
+.global isr21
+isr21:
     push $0
     push $21
     jmp isr_common_stub
 
 # 22: Reserved
-isr22: 
+.global isr22
+isr22:
     push $0
     push $22
     jmp isr_common_stub
 
 # 23: Reserved
-isr23: 
+.global isr23
+isr23:
     push $0
     push $23
     jmp isr_common_stub
 
 # 24: Reserved
-isr24: 
+.global isr24
+isr24:
     push $0
     push $24
     jmp isr_common_stub
 
 # 25: Reserved
-isr25: 
+.global isr25
+isr25:
     push $0
     push $25
     jmp isr_common_stub
 
 # 26: Reserved
-isr26: 
+.global isr26
+isr26:
     push $0
     push $26
     jmp isr_common_stub
 
 # 27: Reserved
-isr27: 
+.global isr27
+isr27:
     push $0
     push $27
     jmp isr_common_stub
 
 # 28: Reserved
-isr28: 
+.global isr28
+isr28:
     push $0
     push $28
     jmp isr_common_stub
 
 # 29: Reserved
-isr29: 
+.global isr29
+isr29:
     push $0
     push $29
     jmp isr_common_stub
 
 # 30: Reserved (With Error Code!)
-isr30: 
+.global isr30
+isr30:
     push $30
     jmp isr_common_stub
 
 # 31: Reserved
-isr31: 
+.global isr31
+isr31:
     push $0
     push $31
     jmp isr_common_stub
 
+# All of the following Interrupt Requests are
+# stubs for hardware interrupts such as device interrupts.
+# These are not exceptions. Each Interrupt Request pushes
+# the IRQ value (starting at 0) along with their
+# corresponding hardware interrupt value (starting at 32).
+#
 # IRQ handlers
-irq0: 
-            push $0
+# System timer (PIT)
+.global irq0
+irq0:
+        push $0
         push $32
         jmp irq_common_stub
-
-irq1: 
-            push $1
+# PS2 Keyboard
+.global irq1
+irq1:
+        push $1
         push $33
         jmp irq_common_stub
-
-irq2: 
-            push $2
+# PIC2
+.global irq2
+irq2:
+        push $2
         push $34
         jmp irq_common_stub
-
-irq3: 
-            push $3
+# COM2
+.global irq3
+irq3:
+        push $3
         push $35
         jmp irq_common_stub
-
-irq4: 
-            push $4
+# COM1
+.global irq4
+irq4:
+        push $4
         push $36
         jmp irq_common_stub
-
-irq5: 
-            push $5
+# LPT2
+.global irq5
+irq5:
+        push $5
         push $37
         jmp irq_common_stub
-
-irq6: 
-            push $6
+# Floppy Disk Drive
+.global irq6
+irq6:
+        push $6
         push $38
         jmp irq_common_stub
-
-irq7: 
-            push $7
+# LPT1
+.global irq7
+irq7:
+        push $7
         push $39
         jmp irq_common_stub
-
-irq8: 
-            push $8
+# CMOS Real Time Clock
+.global irq8
+irq8:
+        push $8
         push $40
         jmp irq_common_stub
-
-irq9: 
-            push $9
+# Unused / Generic
+.global irq9
+irq9:
+        push $9
         push $41
         jmp irq_common_stub
-
-irq10: 
-            push $10
+# Unused / Generic
+.global irq10
+irq10:
+        push $10
         push $42
         jmp irq_common_stub
-
-irq11: 
-            push $11
+# Unused / Generic
+.global irq11
+irq11:
+        push $11
         push $43
         jmp irq_common_stub
-
-irq12: 
-            push $12
+# PS2 Mouse
+.global irq12
+irq12:
+        push $12
         push $44
         jmp irq_common_stub
-
-irq13: 
-            push $13
+# Numeric Coprocessor
+.global irq13
+irq13:
+        push $13
         push $45
         jmp irq_common_stub
-
-irq14: 
-            push $14
+# IDE0 (HDD)
+.global irq14
+irq14:
+        push $14
         push $46
         jmp irq_common_stub
-
-irq15: 
-            push $15
+# IDE1 (HDD)
+.global irq15
+irq15:
+        push $15
         push $47
         jmp irq_common_stub
-
