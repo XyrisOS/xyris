14b07d7ea09d641e971ebc30899e4a0f093edbe4
Keeton T. Feavel <keetonfeavel@gmail.com>
2020-01-12 19:05:07 -0500

    Moved ISR and IRQ registration to loops

--- kernel/arch/x86/isr.cpp
+++ kernel/arch/x86/isr.cpp
@@ -12,6 +12,12 @@
 #include <arch/x86/isr.hpp>
 // Private array of interrupt handlers
 isr_t interrupt_handlers[256];
+void (* isr_func_ptr[])(void) = { isr0,  isr1,  isr2,  isr3,  isr4,  isr5,  isr6,  isr7,
+                                  isr8,  isr9,  isr10, isr11, isr12, isr13, isr14, isr15,
+                                  isr16, isr17, isr18, isr19, isr20, isr21, isr22, isr23, 
+                                  isr24, isr25, isr26, isr27, isr28, isr29, isr30, isr31 };
+void (* irq_func_ptr[])(void) = { irq0, irq1, irq2, irq3,   irq4,  irq5,  irq6,  irq7,
+                                  irq8, irq9, irq10, irq11, irq12, irq13, irq14, irq15 };
 
 void px_interrupts_disable() {
     px_print_debug("Disabling interrupts", Warning);
@@ -25,42 +31,13 @@ void px_interrupts_enable() {
 /* Can't do this with a loop because we need the address
  * of the function names */
 void px_isr_install() {
-    px_print_debug("Installing the ISR.", Info);
+    px_print_debug("Initializing the IDT...", Info);
     // Set all of the gate addresses
-    // TODO: Add all of the ISRs to a list and make a for loop
-    px_idt_set_gate(0, (uint32_t)isr0);
-    px_idt_set_gate(1, (uint32_t)isr1);
-    px_idt_set_gate(2, (uint32_t)isr2);
-    px_idt_set_gate(3, (uint32_t)isr3);
-    px_idt_set_gate(4, (uint32_t)isr4);
-    px_idt_set_gate(5, (uint32_t)isr5);
-    px_idt_set_gate(6, (uint32_t)isr6);
-    px_idt_set_gate(7, (uint32_t)isr7);
-    px_idt_set_gate(8, (uint32_t)isr8);
-    px_idt_set_gate(9, (uint32_t)isr9);
-    px_idt_set_gate(10, (uint32_t)isr10);
-    px_idt_set_gate(11, (uint32_t)isr11);
-    px_idt_set_gate(12, (uint32_t)isr12);
-    px_idt_set_gate(13, (uint32_t)isr13);
-    px_idt_set_gate(14, (uint32_t)isr14);
-    px_idt_set_gate(15, (uint32_t)isr15);
-    px_idt_set_gate(16, (uint32_t)isr16);
-    px_idt_set_gate(17, (uint32_t)isr17);
-    px_idt_set_gate(18, (uint32_t)isr18);
-    px_idt_set_gate(19, (uint32_t)isr19);
-    px_idt_set_gate(20, (uint32_t)isr20);
-    px_idt_set_gate(21, (uint32_t)isr21);
-    px_idt_set_gate(22, (uint32_t)isr22);
-    px_idt_set_gate(23, (uint32_t)isr23);
-    px_idt_set_gate(24, (uint32_t)isr24);
-    px_idt_set_gate(25, (uint32_t)isr25);
-    px_idt_set_gate(26, (uint32_t)isr26);
-    px_idt_set_gate(27, (uint32_t)isr27);
-    px_idt_set_gate(28, (uint32_t)isr28);
-    px_idt_set_gate(29, (uint32_t)isr29);
-    px_idt_set_gate(30, (uint32_t)isr30);
-    px_idt_set_gate(31, (uint32_t)isr31);
-
+    px_print_debug("Setting the ISRs...", Info);
+    for (int isr = 0; isr < 32; isr++) {
+        px_idt_set_gate(isr, (uint32_t)isr_func_ptr[isr]);
+    }
+    px_print_debug("Remapping the PIC...", Info);
     // Remap the PIC
     px_write_byte(0x20, 0x11);
     px_write_byte(0xA0, 0x11);
@@ -71,29 +48,15 @@ void px_isr_install() {
     px_write_byte(0x21, 0x01);
     px_write_byte(0xA1, 0x01);
     px_write_byte(0x21, 0x0);
-    px_write_byte(0xA1, 0x0); 
-
+    px_write_byte(0xA1, 0x0);
     // Install the IRQs
-    // TODO: Make a list out of this too
-    px_idt_set_gate(32, (uint32_t)irq0);
-    px_idt_set_gate(33, (uint32_t)irq1);
-    px_idt_set_gate(34, (uint32_t)irq2);
-    px_idt_set_gate(35, (uint32_t)irq3);
-    px_idt_set_gate(36, (uint32_t)irq4);
-    px_idt_set_gate(37, (uint32_t)irq5);
-    px_idt_set_gate(38, (uint32_t)irq6);
-    px_idt_set_gate(39, (uint32_t)irq7);
-    px_idt_set_gate(40, (uint32_t)irq8);
-    px_idt_set_gate(41, (uint32_t)irq9);
-    px_idt_set_gate(42, (uint32_t)irq10);
-    px_idt_set_gate(43, (uint32_t)irq11);
-    px_idt_set_gate(44, (uint32_t)irq12);
-    px_idt_set_gate(45, (uint32_t)irq13);
-    px_idt_set_gate(46, (uint32_t)irq14);
-    px_idt_set_gate(47, (uint32_t)irq15);
-
+    px_print_debug("Setting the IRQs...", Info);
+    for (int irq = 32; irq < 48; irq++) {
+        px_idt_set_gate(irq, (uint32_t)irq_func_ptr[irq]);
+    }
     // Load the IDT now that we've registered all of our IDT, IRQ, and ISR addresses
     px_load_idt();
+    px_print_debug("Loaded the IDT.", Success);
 }
 
 extern "C" void px_isr_handler(registers_t *r) {
@@ -108,8 +71,9 @@ extern "C" void px_irq_handler(registers_t *regs) {
     px_set_indicator(Red);
     /* After every interrupt we need to send an EOI to the PICs
      * or they will not send another interrupt again */
-    if (regs->int_num >= 40) px_write_byte(0xA0, 0x20);  /* slave */
-    px_write_byte(0x20, 0x20);                           /* master */
+    if (regs->int_num >= 40) 
+        px_write_byte(0xA0, 0x20);                      /* slave  */
+    px_write_byte(0x20, 0x20);                          /* master */
 
     /* Handle the interrupt in a more modular way */
     if (interrupt_handlers[regs->int_num] != 0) {
