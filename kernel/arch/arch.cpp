deda12c246903085a7d517b01b1f09cf63ddbe18
Keeton Feavel <keetonfeavel@gmail.com>
2020-06-01 22:04:15 -0400

    Added getting CPU model name

--- kernel/arch/arch.cpp
+++ kernel/arch/arch.cpp
@@ -10,16 +10,40 @@
  */
 #include <arch/arch.hpp>
 
-static inline int px_arch_cpuid_vendor(int code, int str[4]) {
-    __asm__ volatile ("cpuid":"=a"(*str),
-                "=b"(*(str+0)),
-                "=d"(*(str+1)),
-                "=c"(*(str+2)):"a"(code));
-    return (int)str[0];
+/**
+ * @brief 
+ * 
+ * @param code 
+ * @param str 
+ * @return int 
+ */
+static inline void px_arch_cpuid(int flag, unsigned long eax, unsigned long ebx, unsigned long ecx, unsigned long edx)
+{
+    __asm__ volatile ("cpuid" : "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx) : "a"(flag));
 }
- 
+
+static inline int px_arch_cpuid(int flag, int regs[4]) {
+    // ECX and EDX are swapped in order to make the strings readable
+    __asm__ volatile ("cpuid" : "=a"(*regs), "=b"(*(regs+1)), "=c"(*(regs+2)), "=d"(*(regs+3)) : "a"(flag));
+    return (int)regs[0];
+}
+
+static inline int px_arch_cpuid_vendor(int flag, int regs[4]) {
+    // ECX and EDX are swapped in order to make the strings readable
+    __asm__ volatile ("cpuid" : "=a"(*regs), "=b"(*(regs+0)), "=d"(*(regs+1)), "=c"(*(regs+2)) : "a"(flag));
+    return (int)regs[0];
+}
+
 const char* const px_cpu_get_vendor() {
-	static char vendor[16] = "UNKNOWN CPU!";
-	px_arch_cpuid_vendor(0, (int*)(vendor));
+	static char vendor[16];
+	px_arch_cpuid_vendor(0, (int *)(vendor));
 	return vendor;
+}
+
+const char* const px_cpu_get_model() {
+    static char model[48];
+	px_arch_cpuid(0x80000002, (int *)(model));
+    px_arch_cpuid(0x80000003, (int *)(model+12));
+    px_arch_cpuid(0x80000004, (int *)(model+32));
+	return model;
 }
