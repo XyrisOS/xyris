893321afd48bc7e9d8ce65ee1738797409d667a0
Keeton Feavel <keetonfeavel@gmail.com>
2020-06-17 22:11:54 -0400

    Moved linked list structure to header

--- sysroot/usr/include/kernel/lib/linked_list.hpp
+++ sysroot/usr/include/kernel/lib/linked_list.hpp
@@ -1,17 +1,31 @@
 /**
  * @file linked_list.hpp
  * @author Keeton Feavel (keetonfeavel@cedarville.edu)
- * @brief
+ * @brief Standard linked list library
  * @version 0.1
  * @date 2020-06-17
  *
  * @copyright Copyright Keeton Feavel et al (c) 2020
- *
+ * Thanks for mrvn on the OSDev Wiki for the implementation:
+ * Linked List Bucket Heap 2013 Goswin von Brederlow <goswin-v-b@web.de>
  */
 
 #ifndef PANIX_LIB_LINKED_LIST
 #define PANIX_LIB_LINKED_LIST
 
 #include <sys/panix.hpp>
+
+typedef struct DList {
+    DList *next;
+    DList *prev;
+} DList;
+
+static inline void dlist_init(DList *dlist);
+static inline void dlist_insert_after(DList *d1, DList *d2);
+static inline void dlist_insert_before(DList *d1, DList *d2);
+static inline void dlist_remove(DList *d);
+static inline void dlist_push(DList **d1p, DList *d2);
+static inline DList* dlist_pop(DList **dp);
+static inline void dlist_remove_from(DList **d1p, DList *d2);
 
 #endif /* PANIX_LIB_LINKED_LIST */
