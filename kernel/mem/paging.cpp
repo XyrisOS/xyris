bf53a358b0045172a0e7488b377e3166a4eb37b9
Micah Switzer <micah.switzer.dev@outlook.com>
2020-06-20 21:34:54 -0500

    Add kernel paging init functionality
    Not bootable yet becuase boot.s need to be updated
    to map in more pages for our future page tables.

--- kernel/mem/paging.cpp
+++ kernel/mem/paging.cpp
@@ -1,7 +1,7 @@
 /**
  * @file paging.cpp
  * @author Keeton Feavel (keetonfeavel@cedarville.edu)
- * @brief http://www.jamesmolloy.co.uk/tutorial_html/6.-Paging.html
+ * @brief
  * @version 0.1
  * @date 2019-11-22
  *
@@ -11,6 +11,20 @@
 
 #include <mem/paging.hpp>
 
+#define KADDR_TO_PHYS(addr) ((addr) - KERNEL_BASE)
+
+#define SET_PAGE_MAPPED(addr) mapped_pages[INDEX_FROM_BIT((addr) & PAGE_ALIGN / PAGE_SIZE)] \
+    |= 1UL << OFFSET_FROM_BIT((addr) / PAGE_SIZE)
+#define SET_PAGE_UNMAPPED(addr) mapped_pages[INDEX_FROM_BIT((addr) & PAGE_ALIGN / PAGE_SIZE)] \
+    &= ~(1UL << OFFSET_FROM_BIT((addr) / PAGE_SIZE))
+
+/* one bit for every page */
+static uint32_t mapped_pages[ADDRESS_SPACE_SIZE / PAGE_SIZE / (sizeof(uint32_t) * 8)];
+
+/* both of these must be page aligned for anything to work right at all */
+static page_directory_t page_dir;
+static px_page_table_t page_tables[PAGE_ENTRIES];
+
 void px_paging_init();
 void px_mem_page_fault(registers_t* regs);
 
@@ -18,22 +32,85 @@ void px_mem_page_fault(registers_t* regs) {
    PANIC(regs);
 }
 
-void px_paging_init() {
-    // Register our page fault handler before we enable paging so that
-    // we can set breakpoints or make a futile attempt to recover.
-    px_register_interrupt_handler(14, px_mem_page_fault);
+static inline void px_map_kern_page_table(uint32_t pd_idx, px_page_table_t *table) {
+    page_dir.tables[pd_idx] = table;
+    page_dir.tablesPhysical[pd_idx] = {
+        .present = 1,
+        .read_write = 1,
+        .usermode = 0,
+        // compute the physical address of this page table
+        // the virtual address is obtained with the & operator and
+        // the offset is applied from the load address of the kernel
+        // we must shift it over 12 bits because we only care about
+        // the highest 20 bits for the page table
+        .table_addr = KADDR_TO_PHYS((uint32_t)table) >> 12
+    };
+}
+
+void px_paging_init_dir() {
+    for (int i = 0; i < PAGE_ENTRIES - 1; i++) {
+        px_map_kern_page_table(i, &page_tables[i]);
+    }
+    // recursivly map the last page table to the page directory
+    px_map_kern_page_table(PAGE_ENTRIES - 1, (px_page_table_t*)&page_dir.tablesPhysical[0]);
+    // store the physical address of the page directory for quick access
+    page_dir.physical_addr = KADDR_TO_PHYS((uint32_t)&page_dir.tablesPhysical[0]);
+}
+
+void px_paging_map_early_mem() {
+   for (uint32_t i = 0; i < 0x100000; i += PAGE_SIZE) {
+        page_tables[0].pages[i / PAGE_SIZE] = {
+            .present = 1,
+            .read_write = 1,
+            .usermode = 0,
+            .frame = KADDR_TO_PHYS(i) >> 12
+        };
+        SET_PAGE_MAPPED(i);
+    }
+}
+
+void px_paging_map_hh_kernel() {
+    for (uint32_t addr = KERNEL_START; addr < KERNEL_END; addr += PAGE_SIZE) {
+        uint32_t pde = addr >> 22;
+        uint32_t pte = (addr >> 12) & 0b1111111111;
+        page_tables[pde].pages[pte] = {
+            .present = 1,
+            .read_write = 1,
+            .usermode = 0,
+            .frame = KADDR_TO_PHYS(addr) >> 12
+        };
+        SET_PAGE_MAPPED(addr);
+    }
+}
+
+static inline void px_set_pd(uint32_t page_directory) {
+    asm volatile("mov %0, %%cr3" :: "b"(page_directory));
 }
 
 static inline void px_paging_enable() {
-  uint32_t cr0;
-  asm volatile("mov %%cr0, %0": "=b"(cr0));
-  cr0 |= 0x80000000;
-  asm volatile("mov %0, %%cr0":: "b"(cr0));
+    uint32_t cr0;
+    asm volatile("mov %%cr0, %0": "=b"(cr0));
+    cr0 |= 0x80000000;
+    asm volatile("mov %0, %%cr0":: "b"(cr0));
 }
 
 static inline void px_paging_disable() {
-  uint32_t cr0;
-  asm volatile("mov %%cr0, %0": "=b"(cr0));
-  cr0 &= ~(0x80000000U);
-  asm volatile("mov %0, %%cr0":: "b"(cr0));
+    uint32_t cr0;
+    asm volatile("mov %%cr0, %0": "=b"(cr0));
+    cr0 &= ~(0x80000000U);
+    asm volatile("mov %0, %%cr0":: "b"(cr0));
 }
+
+void px_paging_init() {
+    // we can set breakpoints or make a futile attempt to recover.
+    px_register_interrupt_handler(14, px_mem_page_fault);
+    // init our structures
+    px_paging_init_dir();
+    px_paging_map_early_mem();
+    px_paging_map_hh_kernel();
+    // use our new set of page tables
+    px_set_pd(page_dir.physical_addr & PAGE_ALIGN);
+    // flush the tlb and we're off to the races!
+    px_paging_enable();
+}
+
