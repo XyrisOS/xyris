7941624d138744885f21abebfb8622e620e43f9c
Keeton Feavel <keetonfeavel@gmail.com>
2020-06-24 22:55:34 -0400

    Cleaned up paging code a bit

--- kernel/mem/paging.cpp
+++ kernel/mem/paging.cpp
@@ -6,7 +6,7 @@
  * @version 0.1
  * @date 2019-11-22
  *
- * @copyright Copyright Keeton Feavel et al (c) 2019
+ * @copyright Copyright Keeton Feavel and Micah Switzer (c) 2019
  *
  */
 
@@ -14,9 +14,10 @@
 
 #define KADDR_TO_PHYS(addr) ((addr) - KERNEL_BASE)
 
-#define SET_BIT(bmp, addr) (bmp)[INDEX_FROM_BIT(((addr) & PAGE_ALIGN) / PAGE_SIZE)] \
+#define SET_BIT_IN_MAP(bmp, addr) (bmp)[INDEX_FROM_BIT(((addr) & PAGE_ALIGN) / PAGE_SIZE)] \
     |= 1UL << OFFSET_FROM_BIT((addr) / PAGE_SIZE)
-#define UNSET_BIT(bpm, addr) (bmp)[INDEX_FROM_BIT(((addr) & PAGE_ALIGN) / PAGE_SIZE)] \
+
+#define UNSET_BIT_IN_MAP(bpm, addr) (bmp)[INDEX_FROM_BIT(((addr) & PAGE_ALIGN) / PAGE_SIZE)] \
     &= ~(1UL << OFFSET_FROM_BIT((addr) / PAGE_SIZE))
 
 #define BITMAP_SIZE (ADDRESS_SPACE_SIZE / PAGE_SIZE / (sizeof(uint32_t) * 8))
@@ -32,13 +33,41 @@ static px_page_table_t *         page_dir_virt[PAGE_ENTRIES];
 static px_page_directory_entry_t page_dir_phys[PAGE_ENTRIES] __attribute__ ((section (".page_tables")));
 static px_page_table_t           page_tables[PAGE_ENTRIES]   __attribute__ ((section (".page_tables")));
 
+// Function prototypes
 void px_paging_init();
+static void px_mem_page_fault(registers_t* regs);
+static inline void px_map_kernel_page_table(uint32_t pd_idx, px_page_table_t *table);
+static void px_paging_init_dir();
+static void px_map_kernel_page(px_virtual_address_t vaddr, uint32_t paddr);
+static void px_paging_map_early_mem();
+static void px_paging_map_hh_kernel();
+static inline void px_set_page_dir(uint32_t page_directory);
+static inline void px_paging_enable();
+static inline void px_paging_disable();
+static int find_free(int seq);
+static int get_next_free_phys_page();
+void* px_get_new_page(uint32_t size);
+
+void px_paging_init() {
+    // we can set breakpoints or make a futile attempt to recover.
+    px_register_interrupt_handler(14, px_mem_page_fault);
+    // init our structures
+    px_paging_init_dir();
+    // identity map the first 1 MiB of RAM
+    px_paging_map_early_mem();
+    // map in our higher-half kernel
+    px_paging_map_hh_kernel();
+    // use our new set of page tables
+    px_set_page_dir(page_dir_addr & PAGE_ALIGN);
+    // flush the tlb and we're off to the races!
+    px_paging_enable();
+}
 
 static void px_mem_page_fault(registers_t* regs) {
    PANIC(regs);
 }
 
-static inline void px_map_kern_page_table(uint32_t pd_idx, px_page_table_t *table) {
+static inline void px_map_kernel_page_table(uint32_t pd_idx, px_page_table_t *table) {
     page_dir_virt[pd_idx] = table;
     page_dir_phys[pd_idx] = {
         .present = 1,
@@ -54,39 +83,45 @@ static inline void px_map_kern_page_table(uint32_t pd_idx, px_page_table_t *tabl
 }
 
 static void px_paging_init_dir() {
+    // For every page in kernel memory
     for (int i = 0; i < PAGE_ENTRIES - 1; i++) {
-        px_map_kern_page_table(i, &page_tables[i]);
+        px_map_kernel_page_table(i, &page_tables[i]);
     }
     // recursivly map the last page table to the page directory
-    px_map_kern_page_table(PAGE_ENTRIES - 1, (px_page_table_t*)&page_dir_phys[0]);
+    px_map_kernel_page_table(PAGE_ENTRIES - 1, (px_page_table_t*)&page_dir_phys[0]);
     // store the physical address of the page directory for quick access
     page_dir_addr = KADDR_TO_PHYS((uint32_t)&page_dir_phys[0]);
 }
 
-static void px_map_kern_page(px_virtual_address_t vaddr, uint32_t paddr) {
+static void px_map_kernel_page(px_virtual_address_t vaddr, uint32_t paddr) {
+    // Set the page directory entry (pde) and page table entry (pte)
     uint32_t pde = vaddr.page_dir_index;
     uint32_t pte = vaddr.page_table_index;
+    // If the page's virtual address is not aligned
     if (vaddr.page_offset) {
-        PANIC("attempted to map a non-page-aligned virtual address");
+        PANIC("Attempted to map a non-page-aligned virtual address.");
     }
+    // If the page is already mapped into memory
     if (page_tables[pde].pages[pte].present) {
-        PANIC("attempted to map already mapped page");
+        PANIC("Attempted to map already mapped page.");
     }
+    // Set the page information
     page_tables[pde].pages[pte] = {
-        .present = 1,
-        .read_write = 1,
-        .usermode = 0,
-        .frame = paddr >> 12
+        .present = 1,           // The page is present
+        .read_write = 1,        // The page has r/w permissions
+        .usermode = 0,          // These are kernel pages
+        .frame = paddr >> 12    // The last 12 bits are the frame
     };
-    SET_BIT(mapped_mem, paddr);
-    SET_BIT(mapped_pages, vaddr.val);
+    // Set the associated bit in the bitmaps
+    SET_BIT_IN_MAP(mapped_mem, paddr);
+    SET_BIT_IN_MAP(mapped_pages, vaddr.val);
 }
 
 static void px_paging_map_early_mem() {
     px_virtual_address_t a;
     for (a = VADDR(0); a.val < 0x100000; a.val += PAGE_SIZE) {
         // identity map the early memory
-        px_map_kern_page(a, a.val);
+        px_map_kernel_page(a, a.val);
     }
 }
 
@@ -94,12 +129,12 @@ static void px_paging_map_hh_kernel() {
     px_virtual_address_t addr;
     for (addr = VADDR(KERNEL_START); addr.val < KERNEL_END; addr.val += PAGE_SIZE) {
         // map the higher-half kernel in
-        px_map_kern_page(addr, KADDR_TO_PHYS(addr.val));
+        px_map_kernel_page(addr, KADDR_TO_PHYS(addr.val));
     }
 }
 
-static inline void px_set_pd(uint32_t page_directory) {
-    asm volatile("mov %0, %%cr3" :: "b"(page_directory));
+static inline void px_set_page_dir(uint32_t page_dir) {
+    asm volatile("mov %0, %%cr3" :: "b"(page_dir));
 }
 
 static inline void px_paging_enable() {
@@ -116,27 +151,11 @@ static inline void px_paging_disable() {
     asm volatile("mov %0, %%cr0":: "b"(cr0));
 }
 
-void px_paging_init() {
-    // we can set breakpoints or make a futile attempt to recover.
-    px_register_interrupt_handler(14, px_mem_page_fault);
-    // init our structures
-    px_paging_init_dir();
-    // identity map the first 1 MiB of RAM
-    px_paging_map_early_mem();
-    // map in our higher-half kernel
-    px_paging_map_hh_kernel();
-    // use our new set of page tables
-    px_set_pd(page_dir_addr & PAGE_ALIGN);
-    // flush the tlb and we're off to the races!
-    px_paging_enable();
-}
-
 /**
  * note: this can't find more than 32 sequential pages
  * @param seq the number of sequential pages to get
  */
-static int find_free(int seq)
-{
+static int find_free(int seq) {
     uint32_t check;
     uint32_t mask = (1 << seq) - 1;
     for (int i = 0; i < (ADDRESS_SPACE_SIZE / PAGE_SIZE); i++) {
@@ -147,8 +166,7 @@ static int find_free(int seq)
     return -1;
 }
 
-static int get_next_free_phys_page()
-{
+static int get_next_free_phys_page() {
     for (int i = 0; i < (ADDRESS_SPACE_SIZE / PAGE_SIZE); i++) {
         if (!((mapped_pages[INDEX_FROM_BIT(i)] >> OFFSET_FROM_BIT(i)) & 1)) return i;
     }
@@ -158,16 +176,14 @@ static int get_next_free_phys_page()
 /**
  * map in a new page. if you request less than one page, you will get exactly one page
  */
-void * px_get_new_page(uint32_t size)
-{
+void* px_get_new_page(uint32_t size) {
     int page_count = (size / PAGE_SIZE) + 1;
     int free_idx = find_free(page_count);
     if (free_idx == -1) return NULL;
     for (int i = free_idx; i < free_idx + page_count; i++) {
         int phys_page_idx = get_next_free_phys_page();
         if (phys_page_idx == -1) return NULL;
-        px_map_kern_page(VADDR((uint32_t)i * PAGE_SIZE), phys_page_idx * PAGE_SIZE);
+        px_map_kernel_page(VADDR((uint32_t)i * PAGE_SIZE), phys_page_idx * PAGE_SIZE);
     }
     return (void *)(free_idx * PAGE_SIZE);
 }
-
