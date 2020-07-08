ffa7e32fbe5f8690ecad33d2fa570f33c1890b91
Micah Switzer <micah.switzer.dev@outlook.com>
2020-07-07 22:13:02 -0400

    Stub new bitmap library

---
+++ kernel/lib/bitmap.cpp
@@ -0,0 +1,3 @@
+#include <lib/bitmap.hpp>
+
+
---
+++ sysroot/usr/include/kernel/lib/bitmap.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file bitmap.hpp
+ * @author Micah Switzer (mswitzer@cedarville.edu)
+ * @brief A basic bitmap implementation
+ * @version 0.3
+ * @date 2020-07-08
+ *
+ * @copyright Copyright Keeton Feavel et al (c) 2020
+ *
+ */
+
+#ifndef PANIX_LIB_BITMAP
+#define PANIX_LIB_BITMAP
+
+
+
+#endif
+
