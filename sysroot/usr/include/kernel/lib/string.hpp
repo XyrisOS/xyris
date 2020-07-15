c6bccc78253ebe623f799ddf23bdc1c1de26a5c3
Keeton T. Feavel <keetonfeavel@gmail.com>
2020-07-14 23:58:51 -0400

    Added blank doxygen for string.hpp

--- sysroot/usr/include/kernel/lib/string.hpp
+++ sysroot/usr/include/kernel/lib/string.hpp
@@ -14,17 +14,83 @@
 
 #include <sys/types.hpp>
 
-// String functions
+/**
+ * @brief 
+ * 
+ * @param s 
+ * @return int 
+ */
 int strlen(const char* s);
+/**
+ * @brief 
+ * 
+ * @param source 
+ * @param destination 
+ */
 void strcpy(const char* source, char* destination);
+/**
+ * @brief 
+ * 
+ * @param s1 
+ * @param s2 
+ * @return char* 
+ */
 char* strcat(const char *s1, const char *s2);
+/**
+ * @brief 
+ * 
+ * @param string 
+ * @return char* 
+ */
 char* to_upper(char* string);
+/**
+ * @brief 
+ * 
+ * @param s 
+ */
 void reverse(char* s);
+/**
+ * @brief 
+ * 
+ * @param n 
+ * @param str 
+ */
 void itoa(int n, char str[]);
-// Memory functions
+/**
+ * @brief 
+ * 
+ * @param bufptr 
+ * @param value 
+ * @param size 
+ * @return void* 
+ */
 void* memset(void* bufptr, int value, size_t size);
+/**
+ * @brief 
+ * 
+ * @param aptr 
+ * @param bptr 
+ * @param size 
+ * @return int 
+ */
 int memcmp(const void* aptr, const void* bptr, size_t size);
+/**
+ * @brief 
+ * 
+ * @param dstptr 
+ * @param srcptr 
+ * @param size 
+ * @return void* 
+ */
 void* memmove(void* dstptr, const void* srcptr, size_t size);
+/**
+ * @brief 
+ * 
+ * @param dstptr 
+ * @param srcptr 
+ * @param size 
+ * @return void* 
+ */
 void* memcpy(void* dstptr, const void* srcptr, size_t size);
 
 #endif /* PANIX_LIB_STRING_HPP */
