580df127d1096cccb5edaee1d6d48dc2f5953f34
Micah Switzer <micah.switzer.dev@outlook.com>
2020-07-18 19:30:23 -0400

    Some ascii art for weary eyes

--- kernel/lib/bitmap.cpp
+++ kernel/lib/bitmap.cpp
@@ -21,3 +21,23 @@ size_t bitmap_find_first_bit_clear(bitmap_t *bitmap, size_t size) {
     }
     return SIZE_T_MAX_VALUE;
 }
+
+/*
+
+ Some bits for my sanity:
+
+ word size = 8
+ size = 8
+ i = 4
+
+ The Range:            /---------\
+ The Bits:      | 1010 0011 | 0101 1100 |
+ Their Indices: |           | 8765 4321 |
+ Their Words:   |     1     |     0     |
+
+ idx  = 4 / 8 = 0
+ ofst = 4 % 8 = 4
+ lo = words[idx] >> ofst = 0101 1100 >> 4 = 0000 0101?
+ hi = words[idx + 1] << (8 - ofst) = 1010 0011 << 4 = 0011 0000?
+
+*/
