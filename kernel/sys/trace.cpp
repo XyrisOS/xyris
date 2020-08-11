4c7726c87308b72b47733c98f307f139e772ebd3
Keeton T. Feavel <keetonfeavel@gmail.com>
2020-08-10 23:02:26 -0400

    Moved stack trace to print in upper right corner

--- kernel/sys/trace.cpp
+++ kernel/sys/trace.cpp
@@ -12,15 +12,20 @@
 #include <sys/trace.hpp>
 #include <lib/stdio.hpp>
 #include <arch/arch.hpp>
+#include <dev/serial/rs232.hpp>
 
 void px_stack_trace(size_t max) {
     // Define our stack
     struct stackframe *stk;
     asm volatile("movl %%ebp, %0" : "=r"(stk) ::);
-    px_kprintf("\nStack trace:\n");
+    px_kprintf("\033[0;%iH \033[31mStack trace:\033[0m\n", (X86_TTY_WIDTH - 16));
+    px_rs232_print("\nStack trace:\n");
+    char buf[32];
     for (size_t frame = 0; stk != NULL && frame < max; ++frame) {
         // Unwind to previous stack frame
-        px_kprintf("0x%08X\n", stk->eip);
+        px_ksprintf(buf, "0x%08X\n", stk->eip);
+        px_kprintf("\033[%i;%iH  %s", (frame + 1), (X86_TTY_WIDTH - 16), buf);
+        px_rs232_print(buf);
         stk = stk->ebp;
     }
 }
