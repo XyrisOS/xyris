d7fbed0c01c77832a4c1f146f57c34a544a59d69
Keeton T. Feavel <keetonfeavel@gmail.com>
2020-08-23 16:52:44 -0400

    Fixed attribution for heap implementation.

--- kernel/mem/heap.cpp
+++ kernel/mem/heap.cpp
@@ -1,11 +1,16 @@
 /**
  * @file heap.cpp
+ * @author Goswin von Brederlow (goswin-v-b@web.de)
  * @author Keeton Feavel (keetonfeavel@cedarville.edu)
  * @brief
  * @version 0.3
  * @date 2019-11-22
  *
  * @copyright Copyright the Panix Contributors (c) 2019
+ * Code shamelessly taken from the OSDev Wiki. The article
+ * can be found at the link below.
+ * 
+ * https://wiki.osdev.org/User:Mrvn/LinkedListBucketHeapImplementation
  *
  */
 
--- sysroot/usr/include/kernel/mem/heap.hpp
+++ sysroot/usr/include/kernel/mem/heap.hpp
@@ -1,13 +1,16 @@
 /**
  * @file heap.hpp
+ * @author Goswin von Brederlow (goswin-v-b@web.de)
  * @author Keeton Feavel (keetonfeavel@cedarville.edu)
  * @brief
  * @version 0.3
  * @date 2019-11-22
  *
  * @copyright Copyright the Panix Contributors (c) 2019
- * Thanks for mrvn on the OSDev Wiki for the implementation:
- * Linked List Bucket Heap 2013 Goswin von Brederlow <goswin-v-b@web.de>
+ * Code shamelessly taken from the OSDev Wiki. The article
+ * can be found at the link below.
+ * 
+ * https://wiki.osdev.org/User:Mrvn/LinkedListBucketHeapImplementation
  */
 
 #ifndef PANIX_MEM_HEAP
