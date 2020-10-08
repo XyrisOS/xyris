367e978c3510cca1b2cb11eee5381f2c3d18b612
Keeton T. Feavel <keetonfeavel@gmail.com>
2020-10-07 20:44:53 -0400

    Fixed remaining stubborn whitespace issues

--- kernel/lib/string.cpp
+++ kernel/lib/string.cpp
@@ -65,41 +65,41 @@ void itoa(int n, char str[]) {
 }
 
 void* memset(void* bufptr, int value, size_t size) {
-	unsigned char* buf = (unsigned char*) bufptr;
-	for (size_t i = 0; i < size; i++)
-		buf[i] = (unsigned char) value;
-	return bufptr;
+    unsigned char* buf = (unsigned char*) bufptr;
+    for (size_t i = 0; i < size; i++)
+        buf[i] = (unsigned char) value;
+    return bufptr;
 }
 
 int memcmp(const void* ptr1, const void* ptr2, size_t size) {
-	const unsigned char* a = (const unsigned char*) ptr1;
-	const unsigned char* b = (const unsigned char*) ptr2;
-	for (size_t i = 0; i < size; i++) {
-		if (a[i] < b[i])
-			return -1;
-		else if (a[i] > b[i])
-			return 1;
-	}
-	return 0;
+    const unsigned char* a = (const unsigned char*) ptr1;
+    const unsigned char* b = (const unsigned char*) ptr2;
+    for (size_t i = 0; i < size; i++) {
+        if (a[i] < b[i])
+            return -1;
+        else if (a[i] > b[i])
+            return 1;
+    }
+    return 0;
 }
 
 void* memmove(void* destination, const void* source, size_t size) {
-	unsigned char* dst = (unsigned char*) destination;
-	const unsigned char* src = (const unsigned char*) source;
-	if (dst < src) {
-		for (size_t i = 0; i < size; i++)
-			dst[i] = src[i];
-	} else {
-		for (size_t i = size; i != 0; i--)
-			dst[i-1] = src[i-1];
-	}
-	return destination;
+    unsigned char* dst = (unsigned char*) destination;
+    const unsigned char* src = (const unsigned char*) source;
+    if (dst < src) {
+        for (size_t i = 0; i < size; i++)
+            dst[i] = src[i];
+    } else {
+        for (size_t i = size; i != 0; i--)
+            dst[i-1] = src[i-1];
+    }
+    return destination;
 }
 
 void* memcpy(void* dstptr, const void* srcptr, size_t size) {
-	unsigned char* dst = (unsigned char*) dstptr;
-	const unsigned char* src = (const unsigned char*) srcptr;
-	for (size_t i = 0; i < size; i++)
-		dst[i] = src[i];
-	return dstptr;
+    unsigned char* dst = (unsigned char*) dstptr;
+    const unsigned char* src = (const unsigned char*) srcptr;
+    for (size_t i = 0; i < size; i++)
+        dst[i] = src[i];
+    return dstptr;
 }
