80d60e14aac03e511d1ff546865835faa9f42e3d
Keeton T. Feavel <keetonfeavel@gmail.com>
2020-12-12 14:02:35 -0500

    Fixes incorrect mutex documentation

--- sysroot/usr/include/kernel/lib/mutex.hpp
+++ sysroot/usr/include/kernel/lib/mutex.hpp
@@ -31,16 +31,17 @@ int px_mutex_init(px_mutex_t *mutex);
  */
 int px_mutex_destroy(px_mutex_t *mutex);
 /**
- * @brief Locks a provided mutex.
+ * @brief Locks a provided mutex. This call will block
+ * if the mutex is already locked.
  * 
  * @param mutex Reference mutex
  * @return int Returns 0 on success and -1 on error.
  */
 int px_mutex_lock(px_mutex_t *mutex);
 /**
- * @brief Waits for a mutex to become unlocked and then
- * locks the mutex for use within whatever context this
- * is called.
+ * @brief Attempts to lock a mutex. If the mutex is
+ * currently locked then the function will return and
+ * set errno accordingly.
  * 
  * @param mutex Reference mutex
  * @return int Returns 0 on success and -1 on error.
