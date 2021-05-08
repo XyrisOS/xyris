6a4cb015bf1dc2b769886705704fece291f53933
Keeton Feavel <keetonfeavel@gmail.com>
2021-05-08 18:48:46 -0400

    Update physical memory function naming convention

--- kernel/mem/Physical.cpp
+++ kernel/mem/Physical.cpp
@@ -7,18 +7,20 @@
 #include <lib/assert.hpp>
 #include <lib/debugging.hpp>
 
+namespace Memory {
+
 // TODO: need a mutex to protect access
 static bitmap_t *_phys_map = NULL;
 // hard-coded for now, but can probably be gleaned from boot info
 static size_t _page_count = 1024 * 1024;
 static size_t _first_free = 0;
 
-void px_mem_phys_init(size_t mem_size, uintptr_t kernel_end, px_mmap_iter_t mmap_iter, void *mmap_iter_ctx)
+void PhysicalInit(size_t memSize, uintptr_t kernelEnd, px_mmap_iter_t mapIter, void *mapIterCtx)
 {
-    (void) mem_size;
-    (void) kernel_end;
-    (void) mmap_iter;
-    (void) mmap_iter_ctx;
+    (void) memSize;
+    (void) kernelEnd;
+    (void) mapIter;
+    (void) mapIterCtx;
     // TODO: allocate space for the physical memory bitmap
     // TODO: initialize the bitmap to nothing free
     // TODO: free regions indicated by the bootloader memory map
@@ -27,41 +29,50 @@ void px_mem_phys_init(size_t mem_size, uintptr_t kernel_end, px_mmap_iter_t mmap
     //         a part of the VM initialization
 }
 
-size_t px_mem_phys_get_pagesize(void)
+size_t PhysicalGetPageSize(void)
 {
+    // TODO: Make this architecture specific?
     return 4 * 1024;
 }
 
-size_t px_mem_phys_find_free(size_t count)
+size_t PhysicalFindFree(size_t count)
 {
     return bitmap_find_first_range_clear(_phys_map, _first_free, _page_count, count);
 }
 
-void px_mem_phys_free(size_t page, size_t count)
+void PhysicalFree(size_t page, size_t count)
 {
     if (count == 0) {
         WARN("count == 0, doing nothing");
         return;
     }
     // TODO: optimize this
-    for (size_t i = page; i < page + count; i++)
+    for (size_t i = page; i < page + count; i++) {
         bitmap_clear_bit(_phys_map, i);
+    }
     _first_free = page;
 }
 
-bool px_mem_phys_is_available(size_t page, size_t count)
+bool PhysicalIsAvailable(size_t page, size_t count)
 {
     // TODO: optimize this
-    for (size_t i = page; i < page + count; i++)
-        if (!bitmap_get_bit(_phys_map, i)) return false;
+    for (size_t i = page; i < page + count; i++) {
+        if (!bitmap_get_bit(_phys_map, i)) {
+            return false;
+        }
+    }
     return true;
 }
 
-void px_mem_phys_use(size_t page, size_t count)
+void PhysicalAlloc(size_t page, size_t count)
 {
-    if (page == _first_free)
+    if (page == _first_free) {
         _first_free = page + count;
+    }
     // TODO: optimize this
-    for (size_t i = page; i < page + count; i++)
+    for (size_t i = page; i < page + count; i++) {
         bitmap_set_bit(_phys_map, i);
+    }
+}
+
 }
--- sysroot/usr/include/kernel/mem/Physical.hpp
+++ sysroot/usr/include/kernel/mem/Physical.hpp
@@ -3,29 +3,33 @@
 #include <stddef.h>
 #include <stdint.h>
 
+namespace Memory {
+
 typedef enum {
-    PX_MMAP_AVAILABLE,
-    PX_MMAP_RESERVED,
-    PX_MMAP_ACPI_NV,
-    PX_MMAP_ACPI_RECLAIMABLE
-} px_mmap_type_t;
+    MMAP_AVAILABLE,
+    MMAP_RESERVED,
+    MMAP_ACPI_NV,
+    MMAP_ACPI_RECLAIMABLE
+} MapType;
 
 typedef struct {
     uintptr_t base;
     size_t length;
-    px_mmap_type_t type;
-} px_mmap_entry_t;
+    MapType type;
+} MapEntry;
 
-typedef bool (*px_mmap_iter_t)(px_mmap_entry_t *, void *ctx);
+typedef bool (*px_mmap_iter_t)(MapEntry *, void *ctx);
 
-void px_mem_phys_init(size_t mem_size, uintptr_t kernel_end, px_mmap_iter_t mmap_iter, void *mmap_iter_ctx);
+void PhysicalInit(size_t memSize, uintptr_t kernelEnd, px_mmap_iter_t mapIter, void *mapIterCtx);
 
-size_t px_mem_phys_get_pagesize(void);
+size_t PhysicalGetPageSize(void);
 
-size_t px_mem_phys_find_free(size_t count);
+size_t PhysicalFindFree(size_t count);
 
-void px_mem_phys_free(size_t page, size_t count);
+void PhysicalFree(size_t page, size_t count);
 
-bool px_mem_phys_is_available(size_t page, size_t count);
+bool PhysicalIsAvailable(size_t page, size_t count);
 
-void px_mem_phys_use(size_t page, size_t count);
+void PhysicalAlloc(size_t page, size_t count);
+
+}
