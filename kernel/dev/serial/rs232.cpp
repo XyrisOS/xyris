panix-os-dev-team/Panix

Update ring buffer API to allow static buffers

kernel/dev/serial/rs232.cpp
@@ -19,7 +19,8 @@
 #include <stdarg.h>
 #include <lib/stdio.hpp>
 
-static px_ring_buff_t* read_buffer = NULL;
+static px_ring_buff_t read_buffer;
+uint8_t rs_232_data[1024];
 uint8_t rs_232_line_index;
 uint16_t rs_232_port_base;
 px_mutex_t mutex_rs232("rs232");
@@ -29,6 +30,7 @@ static int px_rs232_is_transmit_empty();
 static char px_rs232_read_char();
 static void px_rs232_write_char(char c);
 static void px_rs232_callback(registers_t *regs);
+static bool px_rs232_init_buffer(int size);
 
 static int px_rs232_received() {
     return px_read_byte(rs_232_port_base + RS_232_LINE_STATUS_REG) & 1;
@@ -94,10 +96,25 @@ static void px_rs232_callback(registers_t *regs) {
     char str[2] = {in, '\0'};
     px_rs232_print(str);
     // Add the character to the circular buffer
-    px_ring_buffer_enqueue(read_buffer, (uint8_t)in);
+    px_ring_buffer_enqueue(&read_buffer, (uint8_t)in);
 }
 
+static bool px_rs232_init_buffer(int size) {
+    bool ret = false;
+    px_mutex_lock(&mutex_rs232);
+    // Initialize the ring buffer
+    ret = (px_ring_buffer_init(&read_buffer, size, rs_232_data) == 0);
+    px_mutex_unlock(&mutex_rs232);
+
+    return ret;
+}
+
+// FIXME: This should take a buffer as an argument so that
+// COM1 & COM2 can have different buffers, or find a way
+// to keep them separate.
 void px_rs232_init(uint16_t com_id) {
+    // Initialize the ring buffer
+    px_rs232_init_buffer(1024);
     // Register the IRQ callback
     rs_232_port_base = com_id;
     uint8_t IRQ = 0x20 + (com_id == RS_232_COM1 ? RS_232_COM1_IRQ : RS_232_COM2_IRQ);
@@ -125,33 +142,11 @@ void px_rs232_init(uint16_t com_id) {
     );
 }
 
-px_ring_buff_t* px_rs232_init_buffer(int size) {
-    px_mutex_lock(&mutex_rs232);
-    // Allocate space for the input buffer
-    read_buffer = (px_ring_buff_t*)malloc(sizeof(px_ring_buff_t));
-    // Initialize the ring buffer
-    if (px_ring_buffer_init(read_buffer, size) == 0) {
-        px_mutex_unlock(&mutex_rs232);
-        return read_buffer;
-    } else {
-        px_mutex_unlock(&mutex_rs232);
-        return NULL;
-    }
-}
-
-px_ring_buff_t* px_rs232_get_buffer() {
-    // Can return NULL. This is documented.
-    return read_buffer;
-}
-
 char px_rs232_get_char() {
     px_mutex_lock(&mutex_rs232);
     // Grab the last byte and convert to a char
     uint8_t data = 0;
-    if (read_buffer != NULL)
-    {
-        px_ring_buffer_dequeue(read_buffer, &data);
-    }
+    px_ring_buffer_dequeue(&read_buffer, &data);
     px_mutex_unlock(&mutex_rs232);
     return (char)data;
 }
@@ -161,9 +156,9 @@ int px_rs232_get_str(char* str, int max) {
     int idx = 0;
     // Keep reading until the buffer is empty or
     // a newline is read.
-    while (!px_ring_buffer_is_empty(read_buffer)) {
+    while (!px_ring_buffer_is_empty(&read_buffer)) {
         uint8_t byte;
-        px_ring_buffer_dequeue(read_buffer, &byte);
+        px_ring_buffer_dequeue(&read_buffer, &byte);
         str[idx] = (char)byte;
         ++idx;
         // Break if it's a newline or null
@@ -183,12 +178,9 @@ int px_rs232_get_str(char* str, int max) {
 }
 
 int px_rs232_close() {
-    px_mutex_lock(&mutex_rs232);
     int ret = -1;
-    if (read_buffer != NULL) {
-        ret = px_ring_buffer_destroy(read_buffer);
-        read_buffer = NULL;
-    }
+    px_mutex_lock(&mutex_rs232);
+    ret = px_ring_buffer_destroy(&read_buffer);
     px_mutex_unlock(&mutex_rs232);
     return ret;
 }

kernel/lib/ring_buffer.cpp
@@ -15,7 +15,7 @@
 #include <mem/heap.hpp>
 #include <lib/errno.h>
 
-int px_ring_buffer_init(px_ring_buff_t* buff, int size) {
+int px_ring_buffer_init(px_ring_buff_t* buff, int size, uint8_t* data) {
     int status = 0;
     // Check if the pointer is valid
     if (buff != NULL) {
@@ -24,12 +24,19 @@ int px_ring_buffer_init(px_ring_buff_t* buff, int size) {
         buff->head = 0;
         buff->tail = 0;
         buff->length = 0;
-        // Allocate the memory for our buffer
-        buff->data = (uint8_t *)malloc(sizeof(uint8_t) * size);
-        // Did we actually allocate the data?
-        if (buff->data == NULL) {
-            status = -1;
-            errno = ENOMEM;
+        // Should we use provided buffer?
+        if (data != NULL) {
+            buff->data = data;
+            buff->external = true;
+        } else {
+            // Allocate the memory for our buffer
+            buff->data = (uint8_t *)malloc(sizeof(uint8_t) * size);
+            buff->external = false;
+            // Did we actually allocate the data?
+            if (buff->data == NULL) {
+                status = -1;
+                errno = ENOMEM;
+            }
         }
     } else {
         status = -1;
@@ -42,8 +49,11 @@ int px_ring_buffer_init(px_ring_buff_t* buff, int size) {
 int px_ring_buffer_destroy(px_ring_buff_t* buff) {
     int status = 0;
     if (buff != NULL) {
-        // Free our data buffer and set the struct to null
-        free(buff->data);
+        // Free the data buffer if self managed
+        if (buff->external == false) {
+            // Free our data buffer and set the struct to null
+            free(buff->data);
+        }
         buff = NULL;
     } else {
         status = -1;

sysroot/usr/include/kernel/lib/ring_buffer.hpp
@@ -20,6 +20,7 @@ typedef struct px_ring_buff {
     int tail;
     int size;
     int length;
+    bool external;
 } px_ring_buff_t;
 
 /**
@@ -28,12 +29,16 @@ typedef struct px_ring_buff {
  * 
  * @param buff Reference circular buffer
  * @param size Maximum buffer capacity (in bytes)
+ * @param data Data buffer to be used. If NULL, a buffer of
+ * size will be allocated and internally managed. If a buffer
+ * is provided, the caller is responsible for memory management
+ * of the provided buffer.
  * @return int Returns 0 on success and -1 on error.
  */
-int px_ring_buffer_init(px_ring_buff_t* buff, int size);
+int px_ring_buffer_init(px_ring_buff_t* buff, int size, uint8_t* data);
 /**
  * @brief Destroys a circular buffer and frees all the
- * data memory.
+ * data memory if the buffer is internally managed.
  * 
  * @param buff Reference circular buffer
  * @return int Returns 0 on success and -1 on error.

XyrisOS/xyris

kernel/dev/serial/rs232.cpp
@@ -19,7 +19,8 @@
 #include <stdarg.h>
 #include <lib/stdio.hpp>
 
-static ring_buff_t* read_buffer = NULL;
+static ring_buff_t read_buffer;
+uint8_t rs_232_data[1024];
 uint8_t rs_232_line_index;
 uint16_t rs_232_port_base;
 mutex_t mutex_rs232("rs232");
@@ -94,9 +95,12 @@ static void rs232_callback(registers_t *regs) {
     char str[2] = {in, '\0'};
     rs232_print(str);
     // Add the character to the circular buffer
-    ring_buffer_enqueue(read_buffer, (uint8_t)in);
+    ring_buffer_enqueue(&read_buffer, (uint8_t)in);
 }
 
+// FIXME: This should take a buffer as an argument so that
+// COM1 & COM2 can have different buffers, or find a way
+// to keep them separate.
 void rs232_init(uint16_t com_id) {
     // Register the IRQ callback
     rs_232_port_base = com_id;
@@ -125,33 +129,11 @@ void rs232_init(uint16_t com_id) {
     );
 }
 
-ring_buff_t* rs232_init_buffer(int size) {
-    mutex_lock(&mutex_rs232);
-    // Allocate space for the input buffer
-    read_buffer = (ring_buff_t*)malloc(sizeof(ring_buff_t));
-    // Initialize the ring buffer
-    if (ring_buffer_init(read_buffer, size) == 0) {
-        mutex_unlock(&mutex_rs232);
-        return read_buffer;
-    } else {
-        mutex_unlock(&mutex_rs232);
-        return NULL;
-    }
-}
-
-ring_buff_t* rs232_get_buffer() {
-    // Can return NULL. This is documented.
-    return read_buffer;
-}
-
 char rs232_get_char() {
     mutex_lock(&mutex_rs232);
     // Grab the last byte and convert to a char
     uint8_t data = 0;
-    if (read_buffer != NULL)
-    {
-        ring_buffer_dequeue(read_buffer, &data);
-    }
+    ring_buffer_dequeue(&read_buffer, &data);
     mutex_unlock(&mutex_rs232);
     return (char)data;
 }
@@ -161,9 +143,9 @@ int rs232_get_str(char* str, int max) {
     int idx = 0;
     // Keep reading until the buffer is empty or
     // a newline is read.
-    while (!ring_buffer_is_empty(read_buffer)) {
+    while (!ring_buffer_is_empty(&read_buffer)) {
         uint8_t byte;
-        ring_buffer_dequeue(read_buffer, &byte);
+        ring_buffer_dequeue(&read_buffer, &byte);
         str[idx] = (char)byte;
         ++idx;
         // Break if it's a newline or null
@@ -183,12 +165,9 @@ int rs232_get_str(char* str, int max) {
 }
 
 int rs232_close() {
-    mutex_lock(&mutex_rs232);
     int ret = -1;
-    if (read_buffer != NULL) {
-        ret = ring_buffer_destroy(read_buffer);
-        read_buffer = NULL;
-    }
+    mutex_lock(&mutex_rs232);
+    ret = ring_buffer_destroy(&read_buffer);
     mutex_unlock(&mutex_rs232);
     return ret;
 }

kernel/lib/ring_buffer.cpp
@@ -15,7 +15,7 @@
 #include <mem/heap.hpp>
 #include <lib/errno.h>
 
-int ring_buffer_init(ring_buff_t* buff, int size) {
+int ring_buffer_init(ring_buff_t* buff, int size, uint8_t* data) {
     int status = 0;
     // Check if the pointer is valid
     if (buff != NULL) {
@@ -24,12 +24,19 @@ int ring_buffer_init(ring_buff_t* buff, int size) {
         buff->head = 0;
         buff->tail = 0;
         buff->length = 0;
-        // Allocate the memory for our buffer
-        buff->data = (uint8_t *)malloc(sizeof(uint8_t) * size);
-        // Did we actually allocate the data?
-        if (buff->data == NULL) {
-            status = -1;
-            errno = ENOMEM;
+        // Should we use provided buffer?
+        if (data != NULL) {
+            buff->data = data;
+            buff->external = true;
+        } else {
+            // Allocate the memory for our buffer
+            buff->data = (uint8_t *)malloc(sizeof(uint8_t) * size);
+            buff->external = false;
+            // Did we actually allocate the data?
+            if (buff->data == NULL) {
+                status = -1;
+                errno = ENOMEM;
+            }
         }
     } else {
         status = -1;
@@ -42,8 +49,11 @@ int ring_buffer_init(ring_buff_t* buff, int size) {
 int ring_buffer_destroy(ring_buff_t* buff) {
     int status = 0;
     if (buff != NULL) {
-        // Free our data buffer and set the struct to null
-        free(buff->data);
+        // Free the data buffer if self managed
+        if (buff->external == false) {
+            // Free our data buffer and set the struct to null
+            free(buff->data);
+        }
         buff = NULL;
     } else {
         status = -1;

sysroot/usr/include/kernel/lib/ring_buffer.hpp
@@ -20,6 +20,7 @@ typedef struct ring_buff {
     int tail;
     int size;
     int length;
+    bool external;
 } ring_buff_t;
 
 /**
@@ -28,9 +29,13 @@ typedef struct ring_buff {
  *
  * @param buff Reference circular buffer
  * @param size Maximum buffer capacity (in bytes)
+ * @param data Data buffer to be used. If NULL, a buffer of
+ * size will be allocated and internally managed. If a buffer
+ * is provided, the caller is responsible for memory management
+ * of the provided buffer.
  * @return int Returns 0 on success and -1 on error.
  */
-int ring_buffer_init(ring_buff_t* buff, int size);
+int ring_buffer_init(ring_buff_t* buff, int size, uint8_t* data);
 /**
  * @brief Destroys a circular buffer and frees all the
  * data memory.