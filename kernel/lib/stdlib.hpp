7df78af45b21f97da6d31b759d0e1e02b088f7e0
JMallone <michels@alunos.utfpr.edu.br>
2021-07-21 23:06:03 -0300

    change header information corretly

--- kernel/lib/stdlib.hpp
+++ kernel/lib/stdlib.hpp
@@ -1,14 +1,22 @@
 /**
- * @file stdlib.hpp
- * @author Michel (JMallone) Gomes (michels@utfpr.edu.br)
- * @brief Standard library, based code from the book "The Standard C Library" - ISBN-10: 0131315099  
- *
+ * @file rand.cpp
+ * @author Brian Kernighan, Dennis Ritchie (C Standard Authors)
+ * Michel (JMallone) Gomes (michels@utfpr.edu.br)
+ * @brief Portable implementation of rand and srand as according to the
+ * C standard implementation by K&R.
  * @version 0.1
  * @date 2021-07-20
  *
- * @copyright 
+ * @copyright C Programming Language copyright Brian Kernighan, Dennis Ritchie
+ * Implementation copyright the Panix Contributors (c) 2021.
+ *
+ * References:
+ *     https://wiki.osdev.org/Random_Number_Generator#The_Standard.27s_Example
+ *     https://pubs.opengroup.org/onlinepubs/9699919799/functions/rand.html
  *
  */
+
+
 #pragma once
 #define MAX_RAND 32767
 /**
