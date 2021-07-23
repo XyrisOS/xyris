753b4948632d86d6f7273ade14ee775ff5a2d03b
Keeton Feavel <keetonfeavel@gmail.com>
2021-07-23 00:41:06 -0400

    Always inline framebuffer getters and setters

--- kernel/dev/vga/fb.hpp
+++ kernel/dev/vga/fb.hpp
@@ -10,6 +10,7 @@
  */
 #pragma once
 #include <stdint.h>
+#include <meta/compiler.hpp>
 
 namespace FB {
 
@@ -30,37 +31,38 @@ public:
                     uint8_t greenMaskSize, uint8_t greenMaskShift,
                     uint8_t blueMaskSize, uint8_t blueMaskShift);
     // Getters
-    void* getAddress()                          { return _addr; }
-    uint32_t getWidth()                         { return _width; }
-    uint32_t getHeight()                        { return _height; }
-    uint16_t getDepth()                         { return _depth; }
-    uint32_t getPitch()                         { return _pitch; }
-    uint8_t getRedMaskSize()                    { return _redMaskSize; }
-    uint8_t getRedMaskShift()                   { return _redMaskShift; }
-    uint8_t getGreenMaskSize()                  { return _greenMaskSize; }
-    uint8_t getGreenMaskShift()                 { return _greenMaskShift; }
-    uint8_t getBlueMaskSize()                   { return _blueMaskSize; }
-    uint8_t getBlueMaskShift()                  { return _blueMaskShift; }
-    FramebufferMemoryModel getModel()           { return _memoryModel; }
+    ALWAYS_INLINE void* getAddress()                          { return _addr; }
+    ALWAYS_INLINE uint32_t getWidth()                         { return _width; }
+    ALWAYS_INLINE uint32_t getHeight()                        { return _height; }
+    ALWAYS_INLINE uint16_t getDepth()                         { return _depth; }
+    ALWAYS_INLINE uint32_t getPitch()                         { return _pitch; }
+    ALWAYS_INLINE uint8_t getRedMaskSize()                    { return _redMaskSize; }
+    ALWAYS_INLINE uint8_t getRedMaskShift()                   { return _redMaskShift; }
+    ALWAYS_INLINE uint8_t getGreenMaskSize()                  { return _greenMaskSize; }
+    ALWAYS_INLINE uint8_t getGreenMaskShift()                 { return _greenMaskShift; }
+    ALWAYS_INLINE uint8_t getBlueMaskSize()                   { return _blueMaskSize; }
+    ALWAYS_INLINE uint8_t getBlueMaskShift()                  { return _blueMaskShift; }
+    ALWAYS_INLINE uint8_t getPixelWidth()                     { return _depth / 8; }
+    ALWAYS_INLINE FramebufferMemoryModel getModel()           { return _memoryModel; }
     // Setters
-    void setWidth(uint16_t val)                 { _width = val; }
-    void setHeight(uint16_t val)                { _height = val; }
-    void setDepth(uint16_t val)                 { _depth = val; }
-    void setPitch(uint16_t val)                 { _pitch = val; }
-    void setRedMaskSize(uint8_t val)            { _redMaskSize = val; }
-    void setRedMaskShift(uint8_t val)           { _redMaskShift = val; }
-    void setGreenMaskSize(uint8_t val)          { _greenMaskSize = val; }
-    void setGreenMaskShift(uint8_t val)         { _greenMaskShift = val; }
-    void setBlueMaskSize(uint8_t val)           { _blueMaskSize = val; }
-    void setBlueMaskShift(uint8_t val)          { _blueMaskShift = val; }
-    void setModel(FramebufferMemoryModel val)   { _memoryModel = val; }
+    ALWAYS_INLINE void setWidth(uint16_t val)                 { _width = val; }
+    ALWAYS_INLINE void setHeight(uint16_t val)                { _height = val; }
+    ALWAYS_INLINE void setDepth(uint16_t val)                 { _depth = val; }
+    ALWAYS_INLINE void setPitch(uint16_t val)                 { _pitch = val; }
+    ALWAYS_INLINE void setRedMaskSize(uint8_t val)            { _redMaskSize = val; }
+    ALWAYS_INLINE void setRedMaskShift(uint8_t val)           { _redMaskShift = val; }
+    ALWAYS_INLINE void setGreenMaskSize(uint8_t val)          { _greenMaskSize = val; }
+    ALWAYS_INLINE void setGreenMaskShift(uint8_t val)         { _greenMaskShift = val; }
+    ALWAYS_INLINE void setBlueMaskSize(uint8_t val)           { _blueMaskSize = val; }
+    ALWAYS_INLINE void setBlueMaskShift(uint8_t val)          { _blueMaskShift = val; }
+    ALWAYS_INLINE void setModel(FramebufferMemoryModel val)   { _memoryModel = val; }
 
 private:
     void* _addr;                                // physical framebuffer address
     uint32_t _width;                            // number pixels on a horizontal line
     uint32_t _height;                           // number horizontal lines present
     uint16_t _depth;                            // color depth (same as bits per pixel)
-    uint32_t _pitch;                            // number bytes of VRAM to skip to go one pixel down
+    uint32_t _pitch;                            // number bytes of VRAM in each line
     uint8_t _redMaskSize;
     uint8_t _redMaskShift;
     uint8_t _greenMaskSize;
