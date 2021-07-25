4a9a7980b2a7b0ab4d8a35a63934abe0200892b8
Micah Switzer <micah.switzer.dev@outlook.com>
2021-07-25 14:21:17 -0400

    Add implementations for the `add` and `delete` C++ operators

---
+++ kernel/meta/cxx.cpp
@@ -0,0 +1,43 @@
+/**
+ * @file cxx.cpp
+ * @author Micah Switzer <mswitzer@cedarville.edu>
+ * @brief C++ dynamic memory management functions
+ * @version 0.1
+ * @date 2021-07-25
+ *
+ * @copyright Copyright the Panix Contributors (c) 2021
+ *
+ * References:
+ *         https://wiki.osdev.org/C++
+ */
+#include <mem/heap.hpp>
+
+void *operator new(size_t size)
+{
+    return malloc(size);
+}
+
+void *operator new [](size_t size)
+{
+    return malloc(size);
+}
+
+void operator delete(void* p)
+{
+    free(p);
+}
+
+void operator delete [](void* p)
+{
+    free(p);
+}
+
+void operator delete(void* p, long unsigned int)
+{
+    free(p);
+}
+
+void operator delete [](void* p, long unsigned int)
+{
+    free(p);
+}
