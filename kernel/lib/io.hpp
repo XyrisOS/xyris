6c273b1496e948c27d624d846c2ed8563cb5db6f
Micah Switzer <micah.switzer.dev@outlook.com>
2021-08-15 09:35:51 -0400

    Correct style for io.hpp and add const where applicable

--- kernel/lib/io.hpp
+++ kernel/lib/io.hpp
@@ -15,26 +15,26 @@ enum whence {
 class Read {
 public:
     virtual ~Read() { }
-    virtual bool isSeekable(void) = 0;
-    virtual size_t tell(void) = 0;
+    virtual bool isSeekable(void) const = 0;
+    virtual size_t tell(void) const = 0;
     virtual bool seek(off_t offset, whence whence) = 0;
-    virtual size_t read(void *ptr, size_t size, size_t nmem) = 0;
-    virtual int error(void) = 0;
-    virtual bool eof(void) = 0;
+    virtual size_t read(void* ptr, size_t size, size_t nmem) = 0;
+    virtual int error(void) const = 0;
+    virtual bool eof(void) const = 0;
 };
 
 class BufReader :
     public Read
 {
 public:
-    BufReader(const void *buf, off_t size)
-        : _buffer((const char *)buf)
+    BufReader(const void* buf, off_t size)
+        : _buffer((const char* )buf)
         , _size(size > 0 ? size : 0)
         , _pos(0) {
     }
     virtual ~BufReader() { }
-    virtual bool isSeekable(void) { return true; }
-    virtual size_t tell(void) { return _pos; }
+    virtual bool isSeekable(void) const { return true; }
+    virtual size_t tell(void) const { return _pos; }
     virtual bool seek(off_t offset, whence whence) {
         off_t target;
         switch (whence) {
@@ -55,7 +55,7 @@ public:
         _pos = target;
         return true;
     }
-    virtual size_t read(void *ptr, size_t size, size_t nmem) {
+    virtual size_t read(void* ptr, size_t size, size_t nmem) {
         off_t count = (off_t)(size * nmem);
         off_t final = _pos + count;
         if (count <= 0 || final < _pos)
@@ -66,11 +66,11 @@ public:
         _pos += count;
         return count;
     }
-    virtual int error(void) { return 0; }
-    virtual bool eof(void) { return _pos == _size; }
+    virtual int error(void) const { return 0; }
+    virtual bool eof(void) const { return _pos == _size; }
 
 private:
-    const char *_buffer;
+    const char* _buffer;
     const off_t _size;
     off_t _pos;
 };
