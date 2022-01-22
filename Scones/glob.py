f6b175ad532462b0d7ee78b9edd2641dd9913ba0
Micah Switzer <micah.switzer.dev@outlook.com>
2022-01-22 11:36:50 -0500

    build: Improve the RecursiveGlob tool
    Before it was unable to handle new directories

--- Scones/glob.py
+++ Scones/glob.py
@@ -1,16 +1,19 @@
-def RecursiveGlob(env, root: str, extensions: list):
-    """
-    Finds all files in a root directory matching the provided file extensions
+class VariantTool:
+    def __init__(self, env):
+        dot = env.Dir('.')
+        self.abs = str(dot.abspath)
+        self.src = str(dot.srcdir)
 
-    Returns a flattened list of File nodes
-    """
-    sources = []
-    for (dirpath, dirs, files) in os.walk(root):
-        for extension in extensions:
-            sources.append(env.Glob(f'{dirpath}/*{extension}'))
-    return env.Flatten(sources)
+    def to_src(self, variant):
+        rel = '.' + variant.removeprefix(self.abs)
+        return os.path.join(self.src, rel)
 
-def RecursiveGlobIgnoreDirs(env, root: str, extensions: list, ignored_dirs: list):
+    def to_variant(self, src):
+        rel = '.' + src.removeprefix(self.src)
+        return os.path.join(self.abs, rel)
+
+
+def recursive_glob(env, root: str, extensions: list, ignored_dirs: list=[]):
     """
     Finds all files in a root directory matching the provided file extensions while
     skipping any file within ignored directories.
@@ -18,17 +21,19 @@ def RecursiveGlobIgnoreDirs(env, root: str, extensions: list, ignored_dirs: list
     Returns a flattened list of File nodes
     """
     sources = []
-    for (dirpath, dirs, files) in os.walk(root):
-        for ignored_dir in ignored_dirs:
-            if not dirpath.startswith(ignored_dir):
-                for extension in extensions:
-                    sources.append(env.Glob(f'{dirpath}/*{extension}'))
-    return env.Flatten(sources)
 
+    rootabs = env.Dir(root).abspath
+    vt = VariantTool(env)
+    extensions = tuple(extensions)
+    for dirpath, dirs, files in os.walk(vt.to_src(rootabs)):
+        if any(ignored_dir in dirpath for ignored_dir in ignored_dirs):
+            continue
+        sources.extend(os.path.normpath(vt.to_variant(os.path.join(dirpath, file))) for file in files if file.endswith(extensions))
+
+    return sources
 
 def generate(env):
-    env.AddMethod(RecursiveGlob, 'RecursiveGlob')
-    env.AddMethod(RecursiveGlobIgnoreDirs, 'RecursiveGlobIgnoreDirs')
+    env.AddMethod(recursive_glob, 'RecursiveGlob')
 
 def exists(env):
     return 1
