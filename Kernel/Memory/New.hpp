acac7af3c076ded33c2d68a9c7bcbbdd366bd045
Keeton Feavel <keeton@xyr.is>
2022-01-25 18:40:43 -0500

    Partially complete `malloc` case 2

---
+++ Kernel/Memory/New.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file New.hpp
+ * @author Keeton Feavel (keeton@xyr.is)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-25
+ *
+ * @copyright Copyright the Xyris Contributors (c) 2022
+ *
+ */
+#pragma once
+#include <stddef.h>
+
+inline void *operator new(size_t, void *p) { return p; }
+inline void *operator new[](size_t, void *p) { return p; }
+inline void  operator delete  (void *, void *) { };
+inline void  operator delete[](void *, void *) { };
