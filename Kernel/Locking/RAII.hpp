f22c857026c8a4925a2f4914630103f0c4a83bc5
Keeton Feavel <keeton@xyr.is>
2022-01-26 22:52:43 -0500

    Add `lockedRegion` RAII mutex lamda to RAII.hpp

--- Kernel/Locking/RAII.hpp
+++ Kernel/Locking/RAII.hpp
@@ -33,3 +33,20 @@ public:
 private:
     Mutex& m_Mutex;
 };
+
+/**
+ * @brief Mutex protected region lambda function. Executed the provided
+ * function after locking the provided mutex. Unlocks before returning.
+ * Provided lambda function cannot return any value, but may return at
+ * any point safely.
+ *
+ * @tparam Function Lambda function template
+ * @param lockedWork Function to be executed in a safe manner
+ * @param mutex Mutex to lock before lambda execution
+ */
+template<typename Function>
+void lockedRegion(Function lockedWork, Mutex& mutex)
+{
+    RAIIMutex raiiMutex(mutex);
+    lockedWork();
+}
