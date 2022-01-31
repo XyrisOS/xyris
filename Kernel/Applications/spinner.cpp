c60bf0e56f9aebfe4a0f963bafc849879943b05b
Keeton Feavel <keeton@xyr.is>
2022-01-30 22:31:24 -0500

    Rework spinner to stress test heap allocation Something is freezing around the point of allocating 3952 bytes

--- Kernel/Applications/spinner.cpp
+++ Kernel/Applications/spinner.cpp
@@ -10,6 +10,9 @@
  */
 #include <Applications/spinner.hpp>
 #include <Devices/Graphics/console.hpp>
+#include <Memory/Heap.hpp>
+#include <Library/string.hpp>
+#include <Arch/Memory.hpp>
 
 namespace Apps {
 
@@ -17,12 +20,23 @@ void spinner(void) {
     Console::printf("\n");
     int i = 0;
     const char spinnay[] = { '|', '/', '-', '\\' };
+    for (size_t size = 1; size < 1024 * 1024 * 1024; size++) {
+        void* ptr = malloc(size);
+        memset(ptr, 0xCAFEBABE, size / sizeof(0xCAFEBABE));
+        // Display a spinner to know that we're still running.
+        Console::printf("\e[s\e[24;0f%c Bytes: %zu\e[u", spinnay[i], size);
+        i = (i + 1) % sizeof(spinnay);
+        free(ptr);
+    }
+    Console::printf("Done!\n");
+/*
     while (true) {
         // Display a spinner to know that we're still running.
         Console::printf("\e[s\e[24;0f%c\e[u", spinnay[i]);
         i = (i + 1) % sizeof(spinnay);
         asm volatile("hlt");
     }
+*/
 }
 
 }
