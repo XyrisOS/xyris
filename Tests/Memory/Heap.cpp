2f6d359db3a3b0e4430cf3bb1cec25b35f50355a
Keeton Feavel <keeton@xyr.is>
2022-02-16 23:09:07 -0500

    Cleanup heap unit test

--- Tests/Memory/Heap.cpp
+++ Tests/Memory/Heap.cpp
@@ -44,69 +44,34 @@ void freePage(void* ptr, size_t n)
 
 } // !namespace Test
 
-static void* testMalloc(unsigned long count)
-{
-    return Test::malloc(count);
-}
-
-static void* testRealloc(void* ptr, unsigned long new_size)
-{
-    return Test::realloc(ptr, new_size);
-}
-
-static void testFree(void* ptr)
-{
-    Test::free(ptr);
-}
-
-typedef struct {
-    void* ptr;
-    size_t len;
-} region;
-
-static size_t pushAt = 0;
-static size_t popAt = 0;
-static region regions[1024];
-
-static void checkDuplicate(void* ptr)
-{
-    for (size_t i = 0; i < pushAt; i++) {
-        REQUIRE(regions[i].ptr != ptr);
-    }
-}
-
-static void* pushRegion(size_t size)
-{
-    void* ptr = testMalloc(size);
-    checkDuplicate(ptr);
-    regions[pushAt++] = {
-        .ptr = ptr,
-        .len = size,
-    };
-
-    if (ptr) {
-        memset(ptr, 'A', size);
-    }
-
-    return ptr;
-}
-
-static void popRegion()
-{
-    testFree(regions[popAt++].ptr);
-}
-
 TEST_CASE("heap stress test", "[heap]")
 {
-    SECTION("constructor")
+    srand(time(NULL));
+
+    SECTION("push all then pop all")
     {
-        const size_t size = 1024;
-        while (pushAt < size) {
-            pushRegion(size);
+        const size_t count = 1024;
+        const size_t allocSize = 1024;
+        void* pointers[count];
+        for (size_t pushAt = 0; pushAt < count; pushAt++) {
+            void* ptr = Test::malloc(allocSize);
+            REQUIRE(ptr != nullptr);
+
+            for (size_t i = 0; i < pushAt; i++) {
+                REQUIRE(pointers[i] != ptr);
+            }
+
+            memset(ptr, 'A', allocSize);
+            pointers[pushAt] = ptr;
         }
 
-        while (popAt < size) {
-            popRegion();
+        for (size_t popAt = 0; popAt < count; popAt++) {
+            void* ptr = pointers[popAt];
+            for (size_t i = 0; i < allocSize; i++) {
+                REQUIRE(((char*)ptr)[i] == 'A');
+            }
+
+            Test::free(ptr);
         }
     }
 }
